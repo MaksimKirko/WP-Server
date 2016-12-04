259d7e869d756daff8c9b568cd345b71d1f8929c
MaximKirko <30max30max@gmail.com>
2016-12-04 15:45:38 +0300

    added ClientInteractionServlet

---
+++ src/main/java/com/github/maximkirko/wpserver/web/controller/ClientInteractionServlet.java
@@ -0,0 +1,63 @@
+package com.github.maximkirko.wpserver.web.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+@WebServlet("/ClientInteractionServlet")
+public class ClientInteractionServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+ 
+    public ClientInteractionServlet() {
+        super();
+ 
+    }
+ 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+ 
+        response.getOutputStream().println("Hurray !! This Servlet Works");
+ 
+    }
+ 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+ 
+        try {
+            int length = request.getContentLength();
+            byte[] input = new byte[length];
+            ServletInputStream sin = request.getInputStream();
+            int c, count = 0 ;
+            while ((c = sin.read(input, count, input.length-count)) != -1) {
+                count +=c;
+            }
+            sin.close();
+ 
+            String recievedString = new String(input);
+            response.setStatus(HttpServletResponse.SC_OK);
+            OutputStreamWriter writer = new OutputStreamWriter(response.getOutputStream());
+ 
+            String resp = "THIS SHIT SI ALSO WORKS!";
+
+            writer.write(resp);
+            writer.flush();
+            writer.close();
+ 
+ 
+ 
+        } catch (IOException e) {
+ 
+ 
+            try{
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                response.getWriter().print(e.getMessage());
+                response.getWriter().close();
+            } catch (IOException ioe) {
+            }
+        }   
+        }
+ 
+}
